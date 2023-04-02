a69816166ce02171dddbdd0b56a7a1c1abfd3d0e
Baqir Naqvi <b.naqvi99@gmail.com>
2023-04-02 09:05:23 +0500

    Fixed Responsiveness _ Styling

--- src/components/TrustedSection/TrustedSection.jsx
+++ src/components/TrustedSection/TrustedSection.jsx
@@ -11,11 +11,11 @@ const TrustedSection = () => {
           Trusted by ~7,700 enterprise customers
         </h4>
         <div className="trusted-img-container ">
-          <img src={iceimg} width="20%" height="20%" />
-          <img src={deloit} width="20%" height="20%" />
-          <img src={iceimg} width="20%" height="20%" />
-          <img src={deloit} width="20%" height="20%" />
-          <img src={iceimg} width="20%" height="20%" />
+          <img src={iceimg} width="20%" height="20%" className="trustImage"/>
+          <img src={deloit} width="20%" height="20%"  className="trustImage"/>
+          <img src={iceimg} width="20%" height="20%"  className="trustImage"/>
+          <img src={deloit} width="20%" height="20%"  className="trustImage"/>
+          <img src={iceimg} width="20%" height="20%"  className="trustImage"/>
 
         </div>
       </Container>
